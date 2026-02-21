bd24cafdc9824cf9f05b1748c65adefb6d7b521e
“Sergey <savershinin23@gmail.com>
2026-02-21 14:26:01 +0300

    first commit keysolo

---
+++ keysolo/task.js
@@ -0,0 +1,114 @@
+class Game {
+  constructor(container) {
+    this.container = container;
+    this.wordElement = container.querySelector('.word');
+    this.winsElement = container.querySelector('.status__wins');
+    this.lossElement = container.querySelector('.status__loss');
+
+    this.reset();
+
+    this.registerEvents();
+  }
+
+  reset() {
+    this.setNewWord();
+    this.winsElement.textContent = 0;
+    this.lossElement.textContent = 0;
+  }
+
+  registerEvents() {
+    document.addEventListener('keydown', e => {
+        if (e.key.toLowerCase() ===  this.currentSymbol.textContent.toLowerCase()) {
+            this.success();
+        } else {
+            this.fail();
+        }
+     });
+  }
+
+  success() {
+    if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
+    this.currentSymbol.classList.add('symbol_correct');
+    this.currentSymbol = this.currentSymbol.nextElementSibling;
+
+    if (this.currentSymbol !== null) {
+      this.currentSymbol.classList.add('symbol_current');
+      return;
+    }
+
+    if (++this.winsElement.textContent === 10) {
+      alert('Победа!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  fail() {
+    if (++this.lossElement.textContent === 5) {
+      alert('Вы проиграли!');
+      this.reset();
+    }
+    this.setNewWord();
+  }
+
+  setNewWord() {
+    const word = this.getWord();
+
+    this.renderWord(word);
+    
+    document.querySelector('.timer')?.remove();
+
+    const N = word.length;
+    const newElement = document.createElement('div');
+    newElement.classList.add('timer');
+    const status = document.querySelector('.status');
+    newElement.textContent = `Таймер обратного отсчёта: ${N} секунд`;
+    
+    status.prepend(newElement);
+
+    const timerId = setInterval(() => {
+        this.fail();
+        clearInterval(timerId);
+    }, N * 1000);
+
+    let lastSeconds = 0;
+    setInterval(() => {
+        lastSeconds += 1;
+        newElement.textContent = `Таймер обратного отсчёта: ${N - lastSeconds} секунд`;
+    } , 1000)
+
+  }
+
+  getWord() {
+    const words = [
+        'bob',
+        'awesome',
+        'netology',
+        'hello',
+        'kitty',
+        'rock',
+        'youtube',
+        'popcorn',
+        'cinema',
+        'love',
+        'javascript'
+      ],
+      index = Math.floor(Math.random() * words.length);
+
+    return words[index];
+  }
+
+  renderWord(word) {
+    const html = [...word]
+      .map(
+        (s, i) =>
+          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+      )
+      .join('');
+    this.wordElement.innerHTML = html;
+
+    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+  }
+}
+
+new Game(document.getElementById('game'))
